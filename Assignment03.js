2c69eb3dd1b3b21b4ae7d0f8268cba5594ac9427
Lý Văn Chính <70565646+chinhlv2101@users.noreply.github.com>
2021-07-06 09:40:25 +0700

    Add files via upload

---
+++ Assignment03.js
@@ -0,0 +1,82 @@
+class Student {
+    constructor(name, mark, gender, subjects) {
+        this.name = name
+        this.mark = mark
+        this.gender = gender
+        this.subjects = subjects
+    }
+}
+
+var s1 = new Student('Chính', 8, 0, [{ title: 'toán', mark: 6 }, { title: 'lý', mark: 7 }])
+var s2 = new Student('Thọ', 6, 0, [{ title: 'toán', mark: 0 }, { title: 'lý', mark: 5 }])
+var s3 = new Student('Nhi', 4.5, 1, [{ tiile: 'toán', mark: 4 }, { title: 'lý', mark: 5 }])
+var s4 = new Student('Quân', 9, 0, [{ title: 'toán', mark: 8 }, { title: 'lý', mark: 9 }])
+var s5 = new Student('Nhung', 8.5, 1, [{ title: 'toán', mark: 8 }, { title: 'lý', mark: 8 }])
+var s6 = new Student('Tiến', 9, 0, [{ title: 'toán', mark: 9.5 }, { title: 'lý', mark: 7 }])
+var s7 = new Student('Yến', 6, 1, [{ title: 'toán', mark: 4 }, { title: 'lý', mark: 3 }])
+var s8 = new Student('Triều', 03, 0, [{ title: 'toán', mark: 3 }, { title: 'lý', mark: 1 }])
+var s9 = new Student('Vinh', 5, 0, [{ title: 'toán', mark: 7 }, { title: 'lý', mark: 2 }])
+var s10 = new Student('Ngọc', 1, 1, [{ title: 'toán', mark: 2 }, { title: 'lý', mark: 1 }])
+
+let array = []
+array.push(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10)
+
+var clone = [...array]
+
+// Duyệt và ghép mảng lại với nhau
+var sort = clone.reduce((init, value) => {
+    var Diem = [value.mark]
+    return init.concat(Diem)
+}, [])
+
+// Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ bé đến lớn
+let sortDecrease = sort.sort((a, b) => {
+    return a - b
+})
+console.log(sortDecrease)
+
+// Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ lớn đến bé
+let sortAscending = sort.sort((a, b) => {
+    return b - a
+})
+console.log(sortAscending)
+
+// Lọc ra danh sách sinh viên là nữ và có điểm tích luỹ lớn hơn 5.0
+let studentFilter = clone.filter((value) => {
+    return value.gender === 1 && value.mark > 5
+})
+console.log(studentFilter)
+
+// Lọc ra danh sách sinh viên có điểm toán hoặc lý lớn hơn 8.0
+let studentSubjects = clone.filter((value) => {
+    return value.subjects[0].mark > 8 || value.subjects[1].mark > 8
+})
+console.log(studentSubjects)
+
+// Duyệt và ghép mảng lại với nhau (subjects)
+let subjectsMinMax = clone.reduce((init, value) => {
+    const result = [value.subjects[0].mark]
+    let a = init.concat(result)
+    return a
+}, [])
+console.log(subjectsMinMax)
+
+// Tìm sinh viên có điểm môn Toán cao nhất và thấp nhất
+const subjectsMax = Math.max(...subjectsMinMax)
+const subjectsMin = Math.min(...subjectsMinMax)
+console.log(`
+Sinh viên có điểm môn toán lớn nhất là: ${subjectsMax}
+Sinh viên có điểm môn toán nhỏ nhất là: ${subjectsMin}
+`)
+
+// Xoá khỏi danh sách sinh viên có điểm tích luỹ là 0
+let deleteStudent = clone.filter((value) => {
+    return value.mark !== 0
+})
+console.log(deleteStudent)
+
+// Thêm 03 sinh viên mới vào danh sách
+var s11 = new Student('TT', 5, 0, [{ title: 'toán', mark: 6 }, { title: 'lý', mark: 6 }])
+var s12 = new Student('VV', 6, 0, [{ title: 'toán', mark: 7 }, { title: 'lý', mark: 5 }])
+var s13 = new Student('HH', 7, 0, [{ title: 'toán', mark: 8 }, { title: 'lý', mark: 8 }])
+
